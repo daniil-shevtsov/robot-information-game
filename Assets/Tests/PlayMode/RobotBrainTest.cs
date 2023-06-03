d9ede6abc10dac55b519c0ed9e8a0fb38578052e
Daniil Shevtsov <me@daniilshevtsov.com>
2023-06-03 12:07:18 +0400

    decided to use god object and script for everything until I know what I am doing

--- Assets/Tests/PlayMode/RobotBrainTest.cs
+++ Assets/Tests/PlayMode/RobotBrainTest.cs
@@ -8,20 +8,39 @@ using UnityEngine.TestTools;
 public class RobotBrainTest
 {
     private GameObject gameObject;
-    private RobotBrain robotBrain;
+    private Player player;
+
+    private FakeTimeWrapper timeWrapper;
+    private FakeInputWrapper inputWrapper;
 
     [OneTimeSetUp]
     public void TestSuitSetup()
     {
         gameObject = MonoBehaviour.Instantiate(
-            Resources.Load<GameObject>("Prefabs/RobotBrain"),
+            Resources.Load<GameObject>("Prefabs/Player"),
             new Vector3(0.0f, 0.0f, 0.0f),
             Quaternion.identity
         );
     }
 
     [SetUp]
-    public void EachTestSetup() { }
+    public void EachTestSetup()
+    {
+        timeWrapper = new FakeTimeWrapper();
+        inputWrapper = new FakeInputWrapper();
+
+        player = gameObject.GetComponent<Player>();
+        player.timeWrapper = timeWrapper;
+        player.inputWrapper = inputWrapper;
+
+        player.fallingSpeed = 2.0f;
+        player.walkingSpeed = 1.5f;
+        player.jumpingSpeed = 3f;
+
+        player.Init();
+        player.transform.position = new Vector3(0f, 0f, 0f);
+        player.transform.localEulerAngles = new Vector3(0f, 0f, 0f);
+    }
 
     [UnityTest]
     public IEnumerator ShouldCompile()
